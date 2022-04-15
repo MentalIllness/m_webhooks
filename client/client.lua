798a44d0231a2541746a4c954f5036ce4c703615
Marso <30401731+danino123@users.noreply.github.com>
2022-04-15 10:35:37 +0300

    Change client function

--- client/client.lua
+++ client/client.lua
@@ -1,5 +1,5 @@
 function SendWebHook(title, description)
-    TriggerEvent('m_webhooks:client:SendWebHook', title, description) -- There is a client event for a security reason ( if you don't want cheaters spam webhooks to your server )
+    TriggerServerEvent('m_webhooks:server:SendWebHook', title, description)
 end
 
 RegisterNetEvent('m_webhooks:client:SendWebHook')
